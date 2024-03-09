fe0fb629378902499411bd64f8370d7b1dc230db
Pankaj Yadav <124139608+pankajyadav05@users.noreply.github.com>
2024-03-09 08:32:11 +0000

    Minnor changes

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const rl = readline.createInterface({
 });
 
 // Ask a question
-rl.question(
-  "You are an aspiring adventurer seeking fame and fortune in the mystical land of Arcadia. While exploring an ancient ruin, you stumble upon a glowing amulet buried deep within a crypt. As you pick it up, a disembodied voice echoes through the chamber: \r\n \r\nBeware, foolish mortal, for you now carry the Cursed Amulet of Zothron. Only by completing three trials can you break the curse and claim its power. Choose wisely, for your decisions will shape your destiny. \r\n",
-  (name) => {
-    log(chalk.blue("Hello, ") + chalk.bgBlue.bold(name));
-    rl.close();
-  }
-);
+rl.question("What is your name? \r\n(Next Line)\r\n", (name) => {
+  log(chalk.blue("Hello, ") + chalk.bgBlue.bold(name));
+  rl.close();
+});
